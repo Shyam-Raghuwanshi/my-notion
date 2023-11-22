d3079be1ad95751c33f382270a881ba9ea473650
shyamraghu <sr9993663832@gmail.com>
2023-11-22 16:10:50 +0530

    Initial commit

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://modest-vulture-85.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: [
+        "files.edgestore.dev"
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
+  
